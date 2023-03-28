f68f602564bbb8f62c5d0270785bf778a4d1ee7e
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2023-03-28 13:11:27 +0400

    Category Update,Delete

--- NotebookTherapyMVC/Areas/Manage/Controllers/CategoryController.cs
+++ NotebookTherapyMVC/Areas/Manage/Controllers/CategoryController.cs
@@ -4,12 +4,14 @@
 public class CategoryController : Controller
 {
     private readonly ICategoryService _categoryService;
+    private readonly IMapper _mapper;
 
-    public CategoryController(ICategoryService categoryService)
-    {
-        _categoryService = categoryService;
-    }
-    public async Task<IActionResult> Index()
+	public CategoryController(ICategoryService categoryService, IMapper mapper)
+	{
+		_categoryService = categoryService;
+		_mapper = mapper;
+	}
+	public async Task<IActionResult> Index()
     {
         var result = await _categoryService.GetAllAsync();
         return View(result);
@@ -22,6 +24,31 @@ public class CategoryController : Controller
             return View();
         }
         var result = await _categoryService.CreateAsync(dto);
+		return RedirectToAction(nameof(Index));
+	}
+
+    [HttpGet]
+	public async Task<IActionResult> Update(int id)
+	{
+		var result = await _categoryService.GetByIdAsync(id);
+        CategoryUpdateDto dto = _mapper.Map<CategoryUpdateDto>(result.Data);
+		return View(dto);
+	}
+	[HttpPost]
+	public async Task<IActionResult> Update(CategoryUpdateDto dto)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(dto);
+		}
+		await _categoryService.UpdateAsync(dto);
+		return RedirectToAction(nameof(Index));
+	}
+	public async Task<IActionResult> Delete(int id)
+	{
+		var result = (await _categoryService.GetByIdAsync(id)).Data;
+		if (result == null) { return RedirectToAction(nameof(Index)); }
+		await _categoryService.SoftDeleteByIdAsync(id);
 		return RedirectToAction(nameof(Index));
 	}
 }
