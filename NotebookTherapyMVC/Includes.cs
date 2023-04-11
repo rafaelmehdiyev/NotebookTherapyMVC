43b7d1be1c7376c1e05ca486dafc7e71c81574cb
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2023-04-11 10:06:21 +0400

    Bundle Get Product Fix

--- NotebookTherapyMVC/Includes.cs
+++ NotebookTherapyMVC/Includes.cs
@@ -7,7 +7,7 @@ public static class Includes
     public static readonly string[] CategoryIncludes = { "Products", "Products.ProductImages", "Products.ProductCollection", "Products.ProductSizes.Size", "Products.ProductBundles.Bundle", "Products.Favourites.User", "Products.Color" };
     public static readonly string[] ProductCollectionIncludes = { "Products" , "Products.ProductImages", "Products.ProductSizes.Size", "Products.ProductBundles.Bundle", "Products.Favourites.User", "Products.Color"};
     public static readonly string[] ColorIncludes = { "Products", "Products.ProductImages", "Products.ProductCollection", "Products.ProductSizes.Size", "Products.ProductBundles.Bundle", "Products.Favourites.User", "Products.Color" };
-    public static readonly string[] BundleIncludes = { "ProductBundles,ProductBundles.Product,ProductBundles.Bundle"};
+    public static readonly string[] BundleIncludes = { "ProductBundles","ProductBundles.Product", "ProductBundles.Product.ProductImages", "ProductBundles.Product.ProductCollection", "ProductBundles.Product.ProductSizes.Size", "ProductBundles.Product.Favourites.User", "ProductBundles.Product.Color" };
     public static readonly string[] ProductIncludes = { "ProductImages", "Category", "ProductCollection", "ProductSizes.Size", "ProductBundles.Bundle", "Favourites.User","Color" };
     public static readonly string[] CartIncludes = { "CartItems.Product.ProductImages", "User" };
     public static readonly string[] CartItemIncludes = { "Product.ProductImages" };
